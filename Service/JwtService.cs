3e160a6f17caf590eb655f984acad290fdaa9e10
YamanKassab24 <yamankas7@gmail.com>
2025-11-05 17:47:36 +0200

    Cleaned nested git repositories

---
+++ Service/JwtService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using TravelDataAccess;
+
+namespace Service
+{
+    public class JwtService
+    {
+        public static string GenerateToken(UserDTO user, string secretKey, string issuer, string audience, int expiryMinutes)
+        {
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserID.ToString()),
+            new Claim(JwtRegisteredClaimNames.UniqueName, (user.Person.FirstName+user.Person.LastName)),
+           new Claim("role", user.Role),
+
+            new Claim(JwtRegisteredClaimNames.Email, user.Person.Email ?? ""),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+    }
+
+    
+}
